dc9ac5e016029729521e43cf9fd3eba3605e682a
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2024-11-21 15:28:26 -0500

    mejoras

--- Laboratorio_no_1/Programa_2.py
+++ Laboratorio_no_1/Programa_2.py
@@ -1,22 +1,37 @@
-with open('test_pr2.txt', 'r') as archivo:
+import re as re
+"""with open('test_pr2.txt', 'r') as archivo:
     contenido = archivo.read()
+
+
 
 # en con espacios
 def en_con_espacios(contenido):
     contador = 0
     for i in range(len(contenido)-3):
-        if contenido[i]+contenido[i+1]+contenido[i+2]+contenido[i+3]==" en ":
+        if contenido[i]+contenido[i+1]+contenido[i+2]+contenido[i+3]==" en " or contenido[i]+contenido[i+1]=="En" :
             contador+=1 
+
     print(contador)
 
 #en sin espacios
 def en_sin_espacios(contenido):
     contador = 0
     for i in range(len(contenido)-3):
-        if contenido[i]+contenido[i+1]=="en":
+        if contenido[i]+contenido[i+1]=="en" or contenido[i]+contenido[i+1]=="En":
             contador+=1 
     print(contador)
 
 
 en_con_espacios(contenido)
-en_sin_espacios(contenido)
+en_sin_espacios(contenido)"""
+
+count=0
+file= open('test_pr2.txt','r')
+for line in file:
+    _en_= re.findall('(\sen\s)',line)
+    #en_ = re.findall("(\s+en\s)", line)
+    count +=len(_en_) #+len(en_)
+
+
+file.close()
+print(count)
